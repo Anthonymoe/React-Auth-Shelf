62f6de0198182228bc981c6f95bb5ac982a34c34
Anthony Moe <anthony.moe7@gmail.com>
2021-04-16 13:53:15 -0500

    get route works

--- src/redux/sagas/item.saga.js
+++ src/redux/sagas/item.saga.js
@@ -8,21 +8,21 @@ function* itemSaga () {
 function* fetchAllItems() {
     // get all items from the DB
     try {
-        const items = yield axios.get('/api/shelf');
+        const items = yield axios.get('./api/shelf');
         console.log('In fetchAllItems() saga generator.', items.data);
         yield put({ type: 'SET_ITEMS', payload: items.data });
     } catch {
-        console.log('get all error');
+        console.log('get all error' );
     }
 }
 
-const items = (state = [], action ) =>{
-    switch (action.type) {
-        case 'SET_ITEMS':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+// const items = (state = [], action ) =>{
+//     switch (action.type) {
+//         case 'SET_ITEMS':
+//             return action.payload;
+//         default:
+//             return state;
+//     }
+// }
 
-export default itemSaga;
+export default fetchAllItems;
